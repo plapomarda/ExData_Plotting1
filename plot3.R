d52439a69e73387457a46046de8ee95df08c1712
plapomarda <plapomarda@yahoo.com>
2015-08-05 06:52:14 -0400

    Updated 3 & 4

--- plot3.R
+++ plot3.R
@@ -10,9 +10,9 @@ power <- subset(powerall,powerall$Date == "2007-02-01" | powerall$Date == "2007-
 # str(power)
 
 par(mfrow=c(1,1))
-plot(power$DateTime,as.numeric(power$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering",)
+plot(power$DateTime,as.numeric(power$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering")
 lines(power$DateTime,as.numeric(power$Sub_metering_2),type="l",col="red")
 lines(power$DateTime,as.numeric(power$Sub_metering_3),type="l",col="blue")
-legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=.8)
 
 
--- plot4.R
+++ plot4.R
@@ -16,7 +16,7 @@ with(power, {
   plot(power$DateTime,as.numeric(power$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering",)
   lines(power$DateTime,as.numeric(power$Sub_metering_2),type="l",col="red")
   lines(power$DateTime,as.numeric(power$Sub_metering_3),type="l",col="blue")
-  legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=.2)
+  legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=.6)
   plot(power$DateTime,as.numeric(power$Global_reactive_power),type="l",xlab="datetime",ylab="Global_reactive_power")
   
 })
